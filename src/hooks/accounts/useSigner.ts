0f199d587d26afc74272edcdb91500338880f27e
Mohammad Shahin <pr.hash2001@gmail.com>
2022-04-18 21:50:39 +0300

    Added a basic implementation of useSigner without Solana

--- src/hooks/accounts/useSigner.ts
+++ src/hooks/accounts/useSigner.ts
@@ -1,2 +1,29 @@
 
-export const useSigner = null;
+import { useSigner as useSignerWagmi } from "wagmi"
+import { useWallet as useWalletSolana } from "@solana/wallet-adapter-react";
+import React from "react";
+
+
+export const useSigner = () => {
+    const [wamgiSignerState, getSignerWagmi] = useSignerWagmi()
+    const solanaInfo = useWalletSolana();
+    
+    const state = React.useMemo(() => {
+        if (wamgiSignerState) 
+            return wamgiSignerState
+        
+        // TODO: return a Solana signer (or create a new type?). It's only used to get the signer's data (to be used in useContract).
+        return null;
+    }, [wamgiSignerState, solanaInfo])
+
+    const getSigner = React.useCallback(() => {
+        if (wamgiSignerState)
+            return getSignerWagmi;
+        
+        // TODO: change this to get the Solana signer (if it exists). This 
+        // was not implemented yet because it's not needed in grants frontend.
+        return () => {}
+    }, [wamgiSignerState, solanaInfo])
+
+    return [state, getSigner] as const
+}
