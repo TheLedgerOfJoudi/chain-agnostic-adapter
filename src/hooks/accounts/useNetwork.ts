4cdb901989f93dee64c510b37c8221233c584cd5
Mohammad Shahin <pr.hash2001@gmail.com>
2022-04-18 21:07:50 +0300

    Configured useNetwork for Solana

--- src/hooks/accounts/useNetwork.ts
+++ src/hooks/accounts/useNetwork.ts
@@ -1,2 +1,30 @@
 
-export const useNetwork = null;
+import { useNetwork as useNetworkWagmi } from "wagmi";
+import { useConnection as useConnectionSolana, useWallet as useWalletSolana } from "@solana/wallet-adapter-react";
+import { CHAIN_ID } from "../../constants";
+
+
+export const useNetwork = () => {
+    const [{ data: wagmiNetworkData, error: wagmiError, loading: wagmiLoading }, switchNetworkWagmi] = useNetworkWagmi();
+    const { connection: solanaConnection } = useConnectionSolana();
+    const solanaInfo = useWalletSolana()
+
+    return [
+        {
+            data: {
+                chain: wagmiNetworkData.chain?.id ? wagmiNetworkData.chain :
+                    (solanaConnection?.rpcEndpoint ? {
+                        id: CHAIN_ID['SOLANA']
+                    } : undefined),
+                chains: [],
+            },
+            // TODO: add solana network network error. 
+            error: wagmiError,
+            loading: wagmiLoading || solanaInfo.connecting || solanaInfo.disconnecting,
+        },
+        // TODO: add solana network switcher. 
+        switchNetworkWagmi
+    ] as const
+};
+
+
